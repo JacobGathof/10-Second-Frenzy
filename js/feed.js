633739b89f0e12f5595ffdce8471530343670cc6
Jacob Gathof <gathofjd@rose-hulman.edu>
2017-10-26 21:25:46 -0400

    Updated the feed page

---
+++ js/feed.js
@@ -0,0 +1,36 @@
+(function () {
+    "use strict";
+
+    function setup() {
+      duplicateNode();
+    }
+
+    $(window).on('load', setup);
+
+    $("#title").on("click", duplicateNode);
+
+    setInterval(reduceTimes, 100);
+
+    function reduceTimes(){
+      let containers = $(".post-container");
+
+      containers.each(function(){
+        let counter = $(this).find("#post-counter");
+        let val = counter.text();
+        let newNum = Number(val) - .1;
+        if(newNum < 0.0){
+          $(this).remove();
+        }
+        counter.text(newNum.toFixed(1));
+      })
+    }
+
+    function duplicateNode(){
+      let node = $("#post-container-template");
+      let node1 = node.clone();
+      node1.addClass("post-container").removeAttr("id");
+      node1.find("#post-counter").text("10");
+      $("#feed-area").append(node1);
+    }
+
+})();
