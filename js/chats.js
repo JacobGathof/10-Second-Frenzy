7aa80ac080c270740cdc8177445dabd2bdd3897b
Chris <nurrencd@rose-hulman.edu>
2017-10-30 20:20:53 -0400

    added chat.js stuff, fixed chat css

--- js/chats.js
+++ js/chats.js
@@ -11,16 +11,19 @@
     let columnLimit = 8;
     console.log("HELLO");
 
-    function createChatBox() {
+    function createChatBox(name) {
         /* Check to see if a window already exists with that person*/
-
+        if ($(`#${name}`).length!=0){
+            return;
+        }
         /* Create the actual chat window */
         const chatWindow = $("<div>").addClass("chat-window");
-        const header = $("<div>").addClass("chat-window-header").text();
+        const header = $("<div>").addClass("chat-window-header").text(name);
         const messageWindow = $("<div>").addClass("chat-message-container");
+        messageWindow.attr("id", name);
         const submitWindow = $("<div>").addClass("send-message-container");
         const textarea = $("<textarea>").addClass("chat-message-entry");
-        const submitButton = $("<div>").addClass("submit-button").attr("rows", "2").attr("cols", "26");
+        const submitButton = $("<div>").addClass("submit-button");
 
         chatWindow.append(header).append(messageWindow).append(submitWindow);
         submitWindow.append(textarea).append(submitButton);
@@ -33,6 +36,15 @@
             columnStart = columnOrigin;
             rowStart += rowLength + rowOffset;
         }
+        /* Add socket connection to the submit buttons*/
+        $.each($(".submit-button"), (index, val) =>{
+            $(val).on('click', function (){
+                socket.emit('chat message', $(".chat-message-entry").text());
+                //set text value to blank
+                return false;
+            });
+        });
+        
 
         /* Append the window to the clobal content container. */
         $(".content-container").append(chatWindow);
@@ -40,8 +52,18 @@
     $(document).ready(function () {
         $.each($(".chat-link"), (index, value) => {
             $(value).on('click', () => {
-                createChatBox($(this).text());
+                createChatBox($(value).text().trim());
             });
         });
     });
+
+    function addIncomingMessage(id, msg){
+        chatWindow = $(id);
+        chatWindow.append($("<div>").addClass("incoming-chat-message").text(msg));
+    }
+
+    function addOutgoingMessage(id, msg){
+        chatWindow = $(id);
+        chatWindow.append($("<div>").addClass("incoming-chat-message").text(msg));
+    }
 })();
