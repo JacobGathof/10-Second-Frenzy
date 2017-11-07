48342856f356bac0201cf44a16cb476b58e6551e
Jacob Gathof <gathofjd@rose-hulman.edu>
2017-11-07 16:22:15 -0500

    Changes to home feed page

--- js/feed.js
+++ js/feed.js
@@ -1,56 +1,45 @@
-$(function () {
-	var socket = io();
-	$('form').submit(function(){
-		socket.emit('chat message', $('#m').val());
-		$('#m').val('');
-		return false;
-	});
-
-	socket.on('chat message', function(msg){
-		$('#messages').append($('<li>').text(msg));
-	});
-	
-	socket.on('destroy', function(msg){
-		$('#messages').children().first().remove();
-	});
-	
-});
 
 
 (function () {
     "use strict";
-
+	
+	
     function setup() {
-      duplicateNode();
+		var socket = io();
+		$('#b').on("click", function(){
+			//socket.emit('chat message', $('#m').val());
+			//$('#m').val('');
+			const name = JSON.parse(sessionStorage.getItem("user"))[0].name;
+			const msg = $("#m").val();
+			console.log(name + " " + msg);
+			createPost(name, msg);
+			return false;
+		});
+
+		socket.on('chat message', function(msg){
+			$('#messages').append($('<li>').text(msg));
+		});
+		
+		socket.on('destroy', function(msg){
+			$('#messages').children().first().remove();
+		});
     }
 
     $(window).on('load', setup);
 
-    $("#title").on("click", duplicateNode);
 
-    setInterval(reduceTimes, 100);
-
-    function reduceTimes(){
-      let containers = $(".post-container");
-
-      containers.each(function(){
-        let counter = $(this).find("#post-counter");
-        let val = counter.text();
-        let newNum = Number(val) - .1;
-        if(newNum < 0.0){
-          $(this).remove();
-        }
-        counter.text(newNum.toFixed(1));
-      })
-    }
-
-    function duplicateNode(){
+    function duplicateNode(name, message){
       let node = $("#post-container-template");
       let node1 = node.clone();
       node1.addClass("post-container").removeAttr("id");
-      node1.find("#post-counter").text("10");
+	  node1.find("#post-content-name").html(name);
+	  node1.find("#post-content-source").html(message);
       $("#feed-area").append(node1);
     }
+	
+	function createPost(name, message){
+		duplicateNode(name, message);
+	}
 	
 	
 })();
