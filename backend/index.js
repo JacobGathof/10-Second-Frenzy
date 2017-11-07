4959fbe7f0c24d0b90a7f842f276fc3c6723e52c
Michael Rusnak <rusnakma@RUSNAKMA-W541.rose-hulman.edu>
2017-11-07 15:57:56 -0500

    A lot of stuff

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const path = require('path');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+require('./models/user');
+const USER = mongoose.model('User');
+
 const port = 3000;
+const users = [];
+let globalId = 0;
+
+const dbURI = 'mongodb://user:user@ds243085.mlab.com:43085/ten-second-frenzy';
+mongoose.connect(dbURI, {
+    useMongoClient: true
+}, (err, res) => {
+    if (err) {
+        console.log(`ERROR connecting to ${dbURI}. ${err}`);
+    } else {
+        console.log(`Successfully connected to ${dbURI}.`);
+    }
+});
 
 app.use('/', express.static(path.join(__dirname, "/../")));
 
@@ -13,42 +30,151 @@ app.get("/", (req, res)=>{
 });
 
 io.on('connection', function(socket){
-    console.log("A user connected");
-    
+
+    socket.on('booyakasha', function(name){
+        addUser(name, socket);
+    });
+
     socket.on('disconnect', function(){
-        console.log('user disconnected');
+        removeUser(socket);
+    });
+
+    socket.on('register', function(name, username, password){
+        console.log("User Registered");
+        USER.create({
+            name: name,
+            username: username,
+            password : password,
+            friends: [],
+        });
+    }); 
+
+    function loginCallback(err, user){
+        if(err){
+            console.log(err);
+        }
+        else{
+            const foundUser = user;
+            console.log(foundUser);
+            socket.emit('sendUserBack', foundUser);
+        }
+    }
+
+    socket.on('login', function(username, password){
+        USER.find({username: username, password: password}, loginCallback);
     });
 });
 
+
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
-        new ChatMessage(0,0,msg);
+        new ChatMessage(socket,msg);
     });
+
 });
+
 
 http.listen(port, function(){
     console.log('listening on *:3000');
 });
 
 
+function addUser(name, socket){
+    users.push(new User(name, socket));
+    console.log("A user connected");
+    console.log(users);
+}
+
+function removeUser(socket){
+    users.splice(users.indexOf(users.filter((u)=>{
+        u.socket.id = socket.id;
+    })[0]), 1);
+    console.log('user disconnected');
+    console.log(users);
+}
+
 class ChatMessage{
 
-    constructor(to, from, msg){
+    constructor(socket, msg){
+        this.id = globalId++;
         this.to = to;
         this.from = from;
         this.msg = msg;
         this.id = 
-        this.sendMessage(msg);
+        this.sendMessage(msg, to, from);
         setTimeout(()=>{
-            this.selfDestruct(this.msg);
+            this.selfDestruct(this.msg, to, from);
         }, 5000);
     }
 
-    sendMessage(msg){
-        io.emit('chat message', msg);
+    sendMessage(msg, to, from){
+        io.to(to).emit('chat message', msg);
+        io.to(from).emit('chat message', msg);
     }
 
-    selfDestruct(msg){
-        io.emit('destroy', msg);
+    selfDestruct(msg, to, from){
+        io.to(to).emit('destroy', msg);
+        io.to(from).emit('destroy', msg);
     }
+}
+
+
+class LocalPost{
+    constructor(socket, to, msg){
+        this.id = globalId++;
+        this.origin = socket;
+        this.to = to;
+        this.msg = msg;
+        this.sendMessage(msg, to);
+        setTimeout(()=>{
+            this.selfDestruct(this.msg, to);
+        }, 5000);
+    }
+
+    sendMessage(msg, to){
+        $.each(to, (f)=>{
+            io.to(f).emit('local post', msg);
+        });
+    }
+
+    selfDestruct(msg, to){
+        $.each(to, (f)=>{
+            io.to(f).emit('destroy local post');
+        });
+    }
+}
+
+
+class ChatConnection{
+    constructor(u){
+        this.users = u;
+    }
+}
+
+
+class User{
+    constructor(username, socket){
+        this.name = username;
+        this.socket = socket;
+        this.friends = [];
+    }
+
+    // post(msg){
+    //     $.each(this.friends, (f)=>{
+    //         new LocalPost(this.socket, this.friends);
+    //     });
+    // }
+
+}
+
+
+function updateUser(name, socketid){
+    USER.findOne({name: name}, (err, usr)=>{
+        if(err){
+            //do nothing
+        }else{
+            usr.socketId = socketid;
+            usr.save();
+        }
+    });
 }
---
+++ js/login.js
@@ -0,0 +1,33 @@
+(function () {
+    const socket = io();
+    $("#new-user-submit").on("click", newUserSubmit);
+    $('#existing-user-submit').on("click", existingUserSubmit);
+
+    function newUserSubmit(){
+        const username = $("#new-username").val();
+        const password = $("#new-password").val();
+        const displayName = $("#new-display-name").val();
+        socket.emit('register', displayName, username, password);
+        $("#new-username").val("");
+        $("#new-password").val("");
+        $("#new-repeated-password").val("");
+        $("#new-display-name").val("");
+        window.alert("You are registered!");
+    }
+
+    function existingUserSubmit(){
+        const username = $("#existing-username").val();
+        const password = $("#existing-password").val();
+        socket.emit('login', username, password);
+    }
+
+    socket.on('sendUserBack', function(user){
+        try{
+            sessionStorage.setItem("user", JSON.stringify(user));
+        }
+        catch(e){
+            console.log(e);
+        }
+    });
+
+})();
