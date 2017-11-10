f54571b27cb24fdefcc663862e7ff259f38e1495
Chris <nurrencd@rose-hulman.edu>
2017-11-10 12:41:03 -0500

    hi

--- frontend/js/leaderboards.js
+++ frontend/js/leaderboards.js
@@ -1,7 +1,6 @@
 (function () {
     $("#most-likes").on("click", mostLikesButtonClicked);
     $("#most-dislikes").on("click", mostDislikesButtonClicked);
-    $("#most-shares").on("click", mostSharesButtonClicked);
     $("#ten-minutes").on("click", tenMinutesButtonClicked);
     $("#ten-hours").on("click", tenHoursButtonClicked);
     $("#ten-days").on("click", tenDaysButtonClicked);
@@ -37,7 +36,7 @@
         const table = $('#posts');
         table.empty();
         const head = $('<tr>').append($('<th>').text('No.')).append($('<th>').text('Author')).append($('<th>').text('Content')).append($('<th>').text('Likes'));
-        table.append();
+        table.append(head);
         for (let i = 0; i < 10; i++){
             const row = $('<tr></tr>');
             row.append($('<td>').text(i+1));
@@ -54,29 +53,5 @@
         $("#most-dislikes").css("background-color", "lightgreen");
         $("#most-shares").css("background-color", "#D3DFD3");
         socket.emit('get least liked posts');
-    }
-
-    function mostSharesButtonClicked(){
-        $("#most-likes").css("background-color", "#D3DFD3");
-        $("#most-dislikes").css("background-color", "#D3DFD3");
-        $("#most-shares").css("background-color", "lightgreen");
-    }
-
-    function tenMinutesButtonClicked(){
-        $("#ten-minutes").css("background-color", "lightgreen");
-        $("#ten-hours").css("background-color", "#D3DFD3");
-        $("#ten-days").css("background-color", "#D3DFD3");
-    }
-
-    function tenHoursButtonClicked(){
-        $("#ten-minutes").css("background-color", "#D3DFD3");
-        $("#ten-hours").css("background-color", "lightgreen");
-        $("#ten-days").css("background-color", "#D3DFD3");
-    }
-
-    function tenDaysButtonClicked(){
-        $("#ten-minutes").css("background-color", "#D3DFD3");
-        $("#ten-hours").css("background-color", "#D3DFD3");
-        $("#ten-days").css("background-color", "lightgreen");
     }
 })();
