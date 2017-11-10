9a029cb4d2425350f19af4e5b52d370388e63465
Chris <nurrencd@rose-hulman.edu>
2017-11-10 12:11:38 -0500

    added leaderboard functionality

--- frontend/js/leaderboards.js
+++ frontend/js/leaderboards.js
@@ -18,8 +18,30 @@
     }
     
     socket.on("most liked posts", function(posts){
-        //display the posts in the html
+        if (posts.length < 10){
+            console.log("ERROR POSTs");
+            return;
+        }
+        populateTable(posts);
     });
+    socket.on("least liked posts", function(posts){
+        if (posts.length < 10){
+            console.log("ERROR POSTS");
+            return;
+        }
+        populateTable(posts);
+    });
+    function populateTable(posts){
+        const table = $('#table')
+        for (let i = 0; i < 10; i++){
+            const row = $('<tr></tr>');
+            row.append($('<td>').text(i+1));
+            row.append($('<td>').text(posts[i].author));
+            row.append($('<td>').text(posts[i].content));
+            row.append($('<td>').text(posts[i].likes));
+            table.append(row);
+        }
+    }
     
     function mostDislikesButtonClicked(){
         $("#most-likes").css("background-color", "#D3DFD3");
